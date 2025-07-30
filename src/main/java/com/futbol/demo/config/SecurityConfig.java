a22eb7853bd0abe6a61bcc1c2fb83e5dc06ddb9e
Cyo <cyosbb@gmail.com>
2025-07-30 16:23:04 +0200

    Front con config.js

--- src/main/java/com/futbol/demo/config/SecurityConfig.java
+++ src/main/java/com/futbol/demo/config/SecurityConfig.java
@@ -117,7 +117,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://futbolink-spring-boot.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         config.setAllowCredentials(true); // Necesario para cookies/tokens
